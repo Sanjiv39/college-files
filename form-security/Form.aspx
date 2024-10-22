fc2112e0d6267d65c3e1d445cb07e719241643e1
Sanjiv39 <sanjiv.samal39@gmail.com>
2024-10-22 22:21:01 +0530

    initial

---
+++ form-security/Form.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Form.aspx.cs" Inherits="project_1.form_security.Form" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:TextBox runat="server" ID="Username"></asp:TextBox>
+        <asp:TextBox runat="server" ID="Password"></asp:TextBox>
+        <asp:Label runat="server" ID="Label1"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ form-security/Form.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+using System.Reflection.Emit;
+
+namespace project_1.form_security
+{
+    public partial class Form : System.Web.UI.Page
+    {
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FormsAuthentication.Authenticate(Username.Text, Password.Text))
+            {
+                FormsAuthentication.RedirectFromLoginPage(Username.Text, true);
+            }
+            else
+            {
+                Label1.Text = "invalid username or password";
+            }
+        }
+    }
+}
